59f848932e33b158182b706571876f0dd1e32d92
Jennifer Adams <ja60@rice.edu>
2020-09-13 00:32:56 -0500

    colors

--- lib/tempresults.dart
+++ lib/tempresults.dart
@@ -10,25 +10,34 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return (Scaffold(
       appBar: AppBar(title: Text('Successful')),
+      
       body: Container(
+        color: Colors.lightBlue[50],
         margin: EdgeInsets.all(10.0),
         alignment: Alignment.center,
+        child: Padding(
+          padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text("Profile", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            Text("", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            Text("Profile", style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
             Text(model.firstName + model.lastName,
-                style: TextStyle(fontSize: 20)),
-            Text(model.school, style: TextStyle(fontSize: 20)),
-            Text(model.email, style: TextStyle(fontSize: 20)),
-            Text(model.pronouns, style: TextStyle(fontSize: 20)),
-            Text("Year:" + model.year.toString(),
-                style: TextStyle(fontSize: 20)),
-            Text(model.major, style: TextStyle(fontSize: 20)),
-            Text(model.bio, style: TextStyle(fontSize: 20)),
+                style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(model.school, style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(model.email, style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(model.pronouns, style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text("Year:" + model.year.toString(), style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(model.major, style: TextStyle(fontSize: 36)),
+            Text("", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Text(model.bio, style: TextStyle(fontSize: 36)),
             Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  padding: const EdgeInsets.symmetric(vertical: 30),
                   child: RaisedButton(
                     color: Colors.blueAccent,
                     onPressed: () {
@@ -43,9 +52,12 @@ class Result extends StatelessWidget {
                       ),
                     ),
                   ),
+            
             ),
+        
           ],
         ),
+      ),
       ),
     ));
   }
